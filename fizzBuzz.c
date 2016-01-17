1960f3ed229376bfeb523f2c3710061ecda2ee85
Jose Roque <jose.roque@mavs.uta.edu>
2016-01-17 17:17:59 -0600

    renamed method

--- fizzBuzz.c
+++ fizzBuzz.c
@@ -1,21 +1,21 @@
 /**
- The famous fillBuzz program which takes a number
+ The famous fizzBuzz program which takes a number
  and determines whether 3 and/or 5 are factors and
- prints fill (for 3) , buzz (for 5), or fill buzz
+ prints fizz (for 3) , buzz (for 5), or fizz buzz
  (for both 3 and 5)
 **/
 #include <stdlib.h>
 #include <stdio.h>
 
-void fillBuzz(int num){
+void fizzBuzz(int num){
 
 	//if 5 and 3 are factors of num
 	if( (num%5==0) && ( num %3 ==0)){
-		printf("fill buzz\n");
+		printf("fizz buzz\n");
 	}
 	//if 3 is a factor of num
 	else if(num %3 ==0){
-		printf("fill\n");
+		printf("fizz\n");
 	}
 	//if 5 is a factor of num
 	else if(num % 5 == 0){
@@ -33,6 +33,6 @@ int main(void){
 	int i;
 
 	for(i=1;i<=15;i++){
-		fillBuzz(i);
+		fizzBuzz(i);
 	}
 }
